dbbeb1ed82b02170d0e6423656be2f709b09cdce
hakonberggren <hakon.berggren.olsen@semcon.com>
2024-03-04 16:35:39 +0100

    Added aliases

---
+++ aliases.sh
@@ -0,0 +1,11 @@
+
+
+alias vim="nvim"
+alias grep='grep --color=auto '
+
+alias gs="git status"
+alias gb="git branch -a"
+alias gbc="git rev-parse --abbrev-ref HEAD" #Git Branch Current
+alias gf="git fetch"
+alias gcm="git commit -m "
+alias gds="git diff --staged"
