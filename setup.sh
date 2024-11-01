a25de63b471b74c7f36b05d71a5f3e02541c10e0
haakools <hakonberggren@gmail.com>
2024-11-01 18:19:49 +0100

    added basic untested setup.sh file

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+# get
+DOTFILES_DIR=$(git rev-parse --show-toplevel)
+
+mkdir -p ~/.config
+
+create_symlink() {
+    local source=$1
+    local target=$2
+    
+    echo "Creating symlink for $target"
+    # -f flag to overwrite existing files without prompting
+    ln -sf "$source" "$target"
+}
+
+# Create .config directory if it doesn't exist
+mkdir -p ~/.config
+
+# Setup nvim configuration
+create_symlink "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
+
+# Setup tmux configuration
+create_symlink "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
+
+echo "Setup completed! Symlinks have been created."
+
