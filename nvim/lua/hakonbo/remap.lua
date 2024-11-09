530ed31c53d0ee78e818adffbbb598117bff4b41
haakools <hakonberggren@gmail.com>
2024-11-09 13:30:48 +0100

    Added :W as save and ctrl+c in visual mode to send to system clipboard

--- nvim/lua/hakonbo/remap.lua
+++ nvim/lua/hakonbo/remap.lua
@@ -9,4 +9,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Keep the same value in register when pasting instead of losing it
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
+-- Also saving on :W because my left pinky gets stuck
+vim.api.nvim_create_user_command("W", "w", {})
+
+
+-- Making ctrl+c ctrl+v work with WSL to other programs
+vim.keymap.set("v", "<C-c>", '"+y', {noremap=true, silent=true})
 
