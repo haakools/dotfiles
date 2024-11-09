b60d436d746bc25bdc1656495ac87cb23eba5ccd
haakools <hakonberggren@gmail.com>
2024-11-09 22:25:29 +0100

    Added gdscript tab over spaces to ftplugin

---
+++ nvim/after/ftplugin/gdscript.lua
@@ -0,0 +1,8 @@
+-- nvim/after/ftplugin/gdscript.lua
+
+-- Set indentation rules for GDScript
+vim.opt_local.tabstop = 4        -- Tabs are 4 spaces wide
+vim.opt_local.shiftwidth = 4     -- Each indent level is 4 spaces
+vim.opt_local.softtabstop = 4    -- A tab inserts 4 spaces
+vim.opt_local.expandtab = false  -- Use actual tabs, not spaces
+
