3bccd01f51b747c226bd63f38763530e6335cd31
Håkon Berggren Olsen <hakon.berggren.olsen@semcon.com>
2024-11-13 08:43:50 +0100

    Added gch git checkout fuzzy finder

--- aliases.sh
+++ aliases.sh
@@ -9,3 +9,11 @@ alias gbc="git rev-parse --abbrev-ref HEAD" #Git Branch Current
 alias gf="git fetch"
 alias gcm="git commit -m "
 alias gds="git diff --staged"
+
+
+# Not really alias but git specific helpers
+# ensure fzf is installed
+
+gch() {
+    git branch -a | fzf | sed 's/^[ *]*//' | xargs -I {} git checkout {}
+}
