5666fb1bd69674265baaf2ff07f98e5b2dd75a78
Håkon Berggren Olsen <nokah@pop-os.tail85ca31.ts.net>
2025-11-24 23:33:10 +0100

    added zoxide search

--- aliases.sh
+++ aliases.sh
@@ -12,9 +12,53 @@ alias gds="git diff --staged"
 alias gba="git branch -a"
 
 
+## Shell utilities - REQUIRES FZF
 # Not really alias but git specific helpers
 # ensure fzf is installed
 
 gch() {
     git branch -a | fzf | sed 's/^[ *]*//' | xargs -I {} git checkout {}
 }
+
+
+# Process killing with fzf
+kp() {
+    # kp -> kill process abbrev
+    # TODO add log statement which processed was killed to stdout
+    ps -aux | fzf | awk '{print $2}' | xargs kill 
+}
+
+
+auto_venv() {
+    # Deactivate if we're currently in a venv
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        deactivate 2>/dev/null
+    fi
+    # Activate if new directory has a venv
+    if [[ -f venv/bin/activate ]]; then
+        source venv/bin/activate
+    fi
+}
+
+# Replace alias with function
+cd() {
+    builtin cd "$@" && auto_venv
+}
+
+# on shell startup (for tmux)
+auto_venv
+
+# requires zoxide 
+# curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/refs/heads/main/install.sh | bash 
+
+eval "$(zoxide init bash)"
+
+# Your custom 'o' function using zoxide
+o() {
+    local dir
+    dir=$(zoxide query -l | fzf --height 40% --reverse --border --preview 'ls -la {}' --preview-window=right:50%:wrap)
+    if [[ -n "$dir" ]]; then
+        cd "$dir"
+    fi
+}
+
